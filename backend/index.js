6f90cc10d18f48660279a96ab87b9deb8bc0fa61
arunparihar <arunsinghparihar3219@gmail.com>
2024-09-01 13:51:06 +0530

    connecting frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://job-junction-six.vercel.app/",
+  origin: "https://job-junction-six.vercel.app",
   credentials: true,
 };
 
